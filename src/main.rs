50bd64e15d5b773146c83f7badcdfe9b34b4c440
kamil-swietoniowski <kamilswietoniowski91@gmail.com>
2026-02-04 14:59:12 +0100

    Improvments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use std::path::Path;
+
+use dirs;
 use rbrain::ui::cli::app;
 
 fn main() {
-    let mut app = app::App::new("rbrain_test.db");
+    let data_dir = dirs::home_dir()
+        .expect("Nie znaleziono katalogu domowego")
+        .join(".local/share/rbrain");
+
+    std::fs::create_dir_all(&data_dir).expect("Nie udało się utworzyć katalogu");
+
+    let db_path = data_dir.join("rbrain.db");
+    let mut app = app::App::new(db_path.to_str().unwrap());
     app.run();
 }
