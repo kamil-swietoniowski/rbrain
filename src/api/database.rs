e65a7b163df839faf40087e5203bc6201e000c91
Kamil Świętoniowski <kamilswietoniowski91@gmail.com>
2026-04-01 17:17:26 +0200

    feat: add get note from database feature

--- src/api/database.rs
+++ src/api/database.rs
@@ -1,6 +1,5 @@
 use rusqlite::{Connection};
-use crate::api::model::{self, Note};
-use chrono::{offset::Utc, DataTime};
+use crate::api::model::Note;
 
 
 const SCHEMA: &str = include_str!("./schema.sql");
@@ -45,10 +44,36 @@ impl Database {
         };
 
         self.conn.execute(
-            "INSERT INTO note (title, content, created_at, modified_at) VALUES (?1, ?2, ?3, ?4)",
+            "INSERT INTO note (title, content, created_at, modified_at) FROM note VALUES (?1, ?2, ?3, ?4)",
             (title, content, modified_at, created_at)
         )?;
         Ok(())
+    }
+    
+    pub fn get_note_from_database(&self, id: i32) -> rusqlite:: Result<Note> {
+        self.conn.query_row(
+            "SELECT id, title, content, modified_at, created_at FROM note WHERE id = ?1",
+            [id],
+            |row| {
+                Ok(Note {
+                    id: row.get(0)?,
+                    title: row.get(1)?,
+                    content: {
+                        let content: Option<String> = row
+                            .get::<_, Option<Vec<u8>>>(2)?
+                            .and_then(|bytes| String::from_utf8(bytes).ok());
+                        content
+                    },
+                    modified_at: {
+                        Some(row.get(3)?)
+                    },
+                    created_at: {
+                        Some(row.get(4)?)
+                    },
+                })
+            },
+        )
+
     }
 
 
